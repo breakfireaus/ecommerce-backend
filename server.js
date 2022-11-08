f72573d70295ebf3187dd29581794210d3ddbccf
breakfireaus <myoungerc@hotmail.com>
2022-11-09 08:34:58 +1100

    updated server.js spelling error

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const routes = require('./routes');
-const sequilize = require('./config/connection');
+const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
