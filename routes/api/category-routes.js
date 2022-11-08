b0eb0021e01b0132648b46bfbee3067cba3bc9f3
breakfireaus <myoungerc@hotmail.com>
2022-11-08 16:22:01 +1100

    updated api route find category by id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -14,9 +14,20 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product, attributes: ['id', 'product_name', 'price', 'stock'] }]
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found using the selected ID!' });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
